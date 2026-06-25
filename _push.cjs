662ab9ed4058f91ec6a461d8d937e6147a7deede
astmain <1311192345@qq.com>
2026-06-26 01:00:30 +0800

    初始化

--- _push.cjs
+++
@@ -1,7 +0,0 @@
-const { execSync } = require('child_process');
-try {
-  execSync('git -C C:/AAA/tool_ui push', { stdio: 'inherit' });
-  console.log('Pushed successfully');
-} catch (e) {
-  console.log('Push exit code:', e.status);
-}
--- ui2/src/store/index.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from 'jotai'
-
-const countAtom = atom(0)
-
-const doubleCountAtom = atom((get) => get(countAtom) * 2)
-
-export { countAtom, doubleCountAtom }
